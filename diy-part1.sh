0cbe6a7b043e21cef7f8946299551185a3be333f
xiaoshv <111450027+xiaoshv@users.noreply.github.com>
2026-01-11 19:34:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # Add ADGuardHome source
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 chmod -R 755 ./package/luci-app-adguardhome/*
